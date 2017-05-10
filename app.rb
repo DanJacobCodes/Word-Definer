616cb794734693d344dcf85ad94698af93dc3d59
Dan Jacob <dansamueljacob@gmail.com>
2017-05-09 19:37:06 -0700

    add a word to the index page once created in the word_form page

--- app.rb
+++ app.rb
@@ -5,4 +5,18 @@ require('./lib/definition')
 require('./lib/word')
 require('pry')
 
-get
+get('/') do
+  @words = Word.all()
+  erb(:index)
+end
+
+get('/new') do
+  erb(:word_form)
+end
+
+post('/') do
+  word = params.fetch('word')
+  Word.new({:word => word}).save()
+ @words = Word.all()
+ erb(:index)
+end
